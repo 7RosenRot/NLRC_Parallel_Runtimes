e2fb4fb1a8307f98b715d1277028cb003a0ec617
unknown <lego.den.2005@mail.ru>
2026-03-29 01:57:39 +0300

    wait-free, tests and markdown still in progress

--- Part_1/workspace/source/counter.cpp
+++ Part_1/workspace/source/counter.cpp
@@ -1,39 +1,27 @@
 #include <counter.hpp>
 
-counter::counter(std::atomic<uint32_t>& low, std::atomic<uint32_t>& high) : low_(low), high_(high) {}
+counter::counter(std::atomic<uint32_t>& high,
+  std::atomic<uint32_t>& low) : high_(high), low_(low) {}
 
-void counter::reset(uint64_t value) {
-  uint32_t low = static_cast<uint32_t>(value);
-  uint32_t high = static_cast<uint32_t>(value >> 32);
+void counter::reset(std::uint64_t value) {
+  uint32_t high = static_cast<uint32_t>(value >> 31);
+  uint32_t low = static_cast<uint32_t>(value & 0x7FFFFFFF);
 
-  low_.store(low, std::memory_order_relaxed);
   high_.store(high, std::memory_order_relaxed);
+  low_.store(low, std::memory_order_relaxed);
 }
 
 uint64_t counter::fetch_add() {
-  while (true) {
-    uint32_t crt_high = high_.load(std::memory_order_acquire);
-    uint32_t crt_low = low_.load(std::memory_order_acquire);
-    
-    uint32_t check = high_.load(std::memory_order_acquire);
+  uint32_t crt_high = high_.load(std::memory_order_acquire);
+  uint32_t old_low = low_.fetch_add(1U, std::memory_order_acq_rel);  
+  
+  uint32_t crt_low = old_low + 1U;
 
-    uint32_t actual_high;
-    if (crt_high == check) {
-      actual_high = crt_high;
-    } else {
-      actual_high = (crt_low < 0x80000000 ? check : crt_high);
-    }
-
-    uint64_t crt_value = (static_cast<uint64_t>(actual_high) << 32) | crt_low;
-
-    uint32_t upd_low = crt_low + 1;
-
-    if (low_.compare_exchange_weak(crt_low, upd_low, std::memory_order_acq_rel)) {
-      if (upd_low == 0) {
-        high_.fetch_add(1, std::memory_order_release);
-      }
-
-      return crt_value;
+  if (crt_low >> 31) {
+    if (low_.compare_exchange_strong(crt_low, crt_low & 0x7FFFFFFF, std::memory_order_acq_rel)) {
+      high_.fetch_add(1U, std::memory_order_release);
     }
   }
+
+  return static_cast<uint64_t>(crt_high) << 31 | static_cast<uint64_t>(old_low & 0x7FFFFFFF);
 }
